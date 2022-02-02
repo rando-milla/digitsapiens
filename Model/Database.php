74771029e1208367d38afcfd50fbde774e8394b7
rando-milla <56270978+rando-milla@users.noreply.github.com>
2022-02-02 04:27:44 +0100

    Initial files

---
+++ Model/Database.php
@@ -0,0 +1,53 @@
+<?php
+class Database
+{
+    protected $connection = null;
+
+    public function __construct()
+    {
+        try {
+            $this->connection = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME);
+
+            if ( mysqli_connect_errno()) {
+                throw new Exception("Could not connect to database.");
+            }
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    public function select($query = "" , $types, $params = [])
+    {
+        try {
+            $stmt = $this->executeStatement( $query , $types, $params );
+            $result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+            $stmt->close();
+
+            return $result;
+        } catch(Exception $e) {
+            throw New Exception( $e->getMessage() );
+        }
+        return false;
+    }
+
+    public function executeStatement($query = "" , $types, $params = [])
+    {
+        try {
+            $stmt = $this->connection->prepare( $query );
+
+            if($stmt === false) {
+                throw New Exception("Unable to do prepared statement: " . $query);
+            }
+
+            if( $params ) {
+                $stmt->bind_param($types, ...$params);
+            }
+
+            $stmt->execute();
+
+            return $stmt;
+        } catch(Exception $e) {
+            throw New Exception( $e->getMessage() );
+        }
+    }
+}
